66f7e7225049aca446549956557561ed932321a0
Kanueven <raelrakeli@gmail.com>
2024-03-29 20:15:11 +0300

    solved roomsdb issues

---
+++ app/src/main/java/com/rae/utibuhealth/data/database/dao/MedicationDao.kt
@@ -0,0 +1,19 @@
+package com.rae.utibuhealth.data.database.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.rae.utibuhealth.data.model.Medication
+
+
+@Dao
+interface MedicineDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMedications(medication: Medication)
+
+    @Query("SELECT * FROM medicine")
+    fun getAllMedicines(): LiveData<List<Medication>>
+}
